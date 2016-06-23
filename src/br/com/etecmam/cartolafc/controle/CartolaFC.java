e62c98194250529a8f14e1b201df41d3cbaafc45
Aloisio Pinto <aloisiop1@gmail.com>
2016-06-22 22:40:39 -0300

    comentei CartolaFC

--- src/br/com/etecmam/cartolafc/controle/CartolaFC.java
+++ src/br/com/etecmam/cartolafc/controle/CartolaFC.java
@@ -7,6 +7,8 @@ import br.com.etecmam.cartolafc.apresentacao.JogadorUI;
 
 public class CartolaFC {
 	
+	//comentário exemplo
+	
 	public static void main(String[] args)
 			throws ClassNotFoundException, 
 			InstantiationException, 
